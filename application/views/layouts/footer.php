11ae1ea2e6b62fe08d6744b5a74a877fef3e02ca
trionoaja <110598353+trionoaja@users.noreply.github.com>
2025-05-05 14:50:36 +0700

    tambahan halaman utama

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,10 @@
+<!-- layouts/footer.php -->
+<footer class="main-footer text-center">
+        <strong>Copyright &copy;
+            <?= date('Y'); ?> Portal Berita</strong>
+    </footer>
+</div>
+<script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/adminlte/js/adminlte.min.js'); ?>"></script>
+</body>
+</html>
---
+++ application/views/layouts/header.php
@@ -0,0 +1,16 @@
+<!-- layouts/header.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Portal Berita</title>
+    <link rel="stylesheet" href="<?= base_url('assets/adminlte/css/adminlte.min.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/fontawesome-free/css/all.min.css'); ?>">
+</head>
+<body class="hold-transition layout-top-nav">
+<div class="wrapper">
+    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+        <a href="<?= base_url(); ?>" class="navbar-brand">
+            <span class="brand-text font-weight-light">Portal Berita</span>
+        </a>
+    </nav>
