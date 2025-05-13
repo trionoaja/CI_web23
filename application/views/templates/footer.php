a2ab0689d0d6ae6b923481c8fef8ffed8e4a96cb
trionoaja <110598353+trionoaja@users.noreply.github.com>
2025-05-13 11:35:51 +0700

    head dan foot datatable

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -52,12 +52,46 @@
 <script src="<?= base_url('assets/adminlte/dist/js/pages/dashboard.js')?>"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?= base_url('assets/adminlte/dist/js/demo.js')?>"></script>
+<!-- DataTables & Export Buttons -->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+
+<!-- Buttons -->
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
 <script>
  $(document).ready(function () {
-        $('#datatable').DataTable({
-            "responsive": true,
-            "autoWidth": true,
-        });
+         $('#datatable').DataTable({
+      responsive: true,
+      lengthChange: true,
+      autoWidth: false,
+      dom: 'Bfrtip',
+      buttons: [
+        {
+          extend: 'excelHtml5',
+          className: 'btn btn-success btn-sm',
+          title: 'Daftar_Berita'
+        },
+        {
+          extend: 'pdfHtml5',
+          className: 'btn btn-danger btn-sm',
+          title: 'Daftar_Berita',
+          orientation: 'landscape',
+          pageSize: 'A4'
+        },
+        {
+          extend: 'print',
+          className: 'btn btn-info btn-sm',
+          title: 'Daftar_Berita'
+        }
+      ]
+    }).buttons().container().appendTo('#datatable_wrapper .col-md-6:eq(0)');
 
         $('.summernote').summernote({
             height: 300,
