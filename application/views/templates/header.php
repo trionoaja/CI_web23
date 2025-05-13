b94be04e88ab21f565ab37aa93070f5a1327e290
trionoaja <trionoaja@gmail.com>
2025-05-13 21:46:37 +0700

    dashboard

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -35,13 +35,10 @@
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
-  <!-- Navbar -->
-  <!-- ... Navbar tetap sama ... -->
-
-  <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="<?= base_url(); ?>" class="brand-link">
-      <img src="<?= base_url('assets/adminlte/dist/img/AdminLTELogo.png'); ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?= base_url('assets/adminlte/dist/img/AdminLTELogo.png'); ?>"
+       alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -49,7 +46,8 @@
     <div class="sidebar">
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url('assets/adminlte/dist/img/user2-160x160.jpg'); ?>" class="img-circle elevation-2" alt="User Image">
+          <img src="<?= base_url('assets/adminlte/dist/img/user2-160x160.jpg'); ?>" 
+          class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
