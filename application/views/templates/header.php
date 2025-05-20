07d1addba694f86810d2b2cf7aadf784ac6f6a20
trionoaja <trionoaja@gmail.com>
2025-05-20 08:03:06 +0700

    re

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -87,11 +87,13 @@
             </ul>
           </li>
 		  <li class="nav-item">
-        <a href="<?= site_url('auth/logout') ?>" class="nav-link">
-            <i class="nav-icon fas fa-sign-out-alt"></i>
-            <p>Logout</p>
-        </a>
-    </li>
+            <a href="<?= base_url('auth/logout'); ?>" class="nav-link">
+              <i class="fas fa-sign-out-alt"></i>
+              <p>
+                Logout
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
